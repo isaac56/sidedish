d164b4ae0ef3445e26ef245e9623b7caf46e4922
정이삭 <isaac56@naver.com>
2021-04-22 02:25:50 +0900

    [#8] Create DummyController
    1. url로 json 데이터를 받기 위한 DummyController 생성
    2. @ConditionalOnProperty를 사용하여 배포 서버에서는 Dummy Import 기능 끌 수 있게 했습니다.

--- BE/sidedish/src/main/java/team16/sidedish/SidedishApplication.java
+++ BE/sidedish/src/main/java/team16/sidedish/SidedishApplication.java
@@ -2,12 +2,14 @@ package team16.sidedish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"team16.dummy"})
 @SpringBootApplication
 public class SidedishApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SidedishApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SidedishApplication.class, args);
+    }
 
 }
