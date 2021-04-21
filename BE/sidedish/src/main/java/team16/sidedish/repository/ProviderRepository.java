7611f531247d2122b99648c3d3d42125d83974d8
정이삭 <isaac56@naver.com>
2021-04-22 01:25:31 +0900

    [#4] feat: Add necessary query method to DAOs

--- BE/sidedish/src/main/java/team16/sidedish/repository/ProviderRepository.java
+++ BE/sidedish/src/main/java/team16/sidedish/repository/ProviderRepository.java
@@ -3,5 +3,8 @@ package team16.sidedish.repository;
 import org.springframework.data.repository.CrudRepository;
 import team16.sidedish.domain.entity.aggregate.provider.Provider;
 
+import java.util.Optional;
+
 public interface ProviderRepository extends CrudRepository<Provider, Long> {
+    Optional<Provider> findByName(String name);
 }
--- BE/sidedish/src/main/java/team16/sidedish/repository/lookup/CategoryRepository.java
+++ BE/sidedish/src/main/java/team16/sidedish/repository/lookup/CategoryRepository.java
@@ -3,5 +3,11 @@ package team16.sidedish.repository.lookup;
 import org.springframework.data.repository.CrudRepository;
 import team16.sidedish.domain.entity.lookUp.Category;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
+    List<Category> findAll();
+
+    Optional<Category> findByName(String name);
 }
