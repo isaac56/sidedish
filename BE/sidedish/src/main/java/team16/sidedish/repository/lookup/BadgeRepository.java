faeb619ad8b42bc7ec86b2b7a9a0dc2a0a7eebf4
정이삭 <isaac56@naver.com>
2021-04-22 01:25:31 +0900

    [#4] feat: Add necessary query method to DAOs

--- BE/sidedish/src/main/java/team16/sidedish/repository/lookup/BadgeRepository.java
+++ BE/sidedish/src/main/java/team16/sidedish/repository/lookup/BadgeRepository.java
@@ -3,5 +3,11 @@ package team16.sidedish.repository.lookup;
 import org.springframework.data.repository.CrudRepository;
 import team16.sidedish.domain.entity.lookUp.Badge;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BadgeRepository extends CrudRepository<Badge, Integer> {
+    List<Badge> findAll();
+
+    Optional<Badge> findByName(String name);
 }
