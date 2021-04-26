3086c04686c023ae6bc80588e9346202673ab3e4
정이삭 <isaac56@naver.com>
2021-04-26 23:35:12 +0900

    [#12] feat: Product 관련 API 구현

--- BE/sidedish/src/main/java/team16/sidedish/controller/ProductController.java
+++ BE/sidedish/src/main/java/team16/sidedish/controller/ProductController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team16.sidedish.domain.enums.CategoryEnum;
 import team16.sidedish.dto.response.BestMenuResponseDto;
 import team16.sidedish.dto.response.MenuResponseDto;
+import team16.sidedish.dto.response.detail.DetailResponseDto;
 import team16.sidedish.dto.response.list.BestMenuListResponseDto;
 import team16.sidedish.dto.response.list.MenuListResponseDto;
 import team16.sidedish.service.ProductService;
@@ -21,28 +23,48 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("/menu")
-    public ResponseEntity<MenuListResponseDto> menuList() {
-        return ResponseEntity.ok(productService.menuList());
-    }
-
-    @GetMapping("/menu/{detail_hash}")
-    public ResponseEntity<MenuResponseDto> menu(@PathVariable("detail_hash") String detailHash) {
-        return ResponseEntity.ok(productService.menu(detailHash));
-    }
-
     @GetMapping("/best")
     public ResponseEntity<BestMenuListResponseDto> bestMenuList() {
-        return ResponseEntity.ok(productService.bestMenuList());
+        return ResponseEntity.ok(productService.getBestAllMenuList());
     }
 
-    @GetMapping("/best/{id}")
-    public ResponseEntity<BestMenuResponseDto> bestMenu(@PathVariable Integer id) {
-        return ResponseEntity.ok(productService.bestMenu(id));
+    @GetMapping("/best/{categoryId}")
+    public ResponseEntity<BestMenuResponseDto> bestMenu(@PathVariable Integer categoryId) {
+        return ResponseEntity.ok(productService.getBestCategoryMenuList(categoryId));
     }
 
     @GetMapping("/main")
-    public ResponseEntity<MenuResponseDto> mainMenu() {
-        return null;
+    public ResponseEntity<MenuListResponseDto> mainMenu() {
+        return ResponseEntity.ok(productService.getMenuList(CategoryEnum.MAIN));
+    }
+
+    @GetMapping("/main/{hash}")
+    public ResponseEntity<MenuResponseDto> mainMenuByHash(@PathVariable String hash) {
+        return ResponseEntity.ok(productService.getMenu(CategoryEnum.MAIN, hash));
+    }
+
+    @GetMapping("/soup")
+    public ResponseEntity<MenuListResponseDto> soupMenu() {
+        return ResponseEntity.ok(productService.getMenuList(CategoryEnum.SOUP));
+    }
+
+    @GetMapping("/soup/{hash}")
+    public ResponseEntity<MenuResponseDto> soupMenuByHash(@PathVariable String hash) {
+        return ResponseEntity.ok(productService.getMenu(CategoryEnum.SOUP, hash));
+    }
+
+    @GetMapping("/side")
+    public ResponseEntity<MenuListResponseDto> sideMenu() {
+        return ResponseEntity.ok(productService.getMenuList(CategoryEnum.SIDE));
+    }
+
+    @GetMapping("/side/{hash}")
+    public ResponseEntity<MenuResponseDto> sideMenuByHash(@PathVariable String hash) {
+        return ResponseEntity.ok(productService.getMenu(CategoryEnum.SIDE, hash));
+    }
+
+    @GetMapping("/detail/{hash}")
+    public ResponseEntity<DetailResponseDto> detailInfo(@PathVariable String hash) {
+        return ResponseEntity.ok(productService.getDetail(hash));
     }
 }
--- BE/sidedish/src/main/java/team16/sidedish/service/ProductService.java
+++ BE/sidedish/src/main/java/team16/sidedish/service/ProductService.java
@@ -4,16 +4,23 @@ import org.springframework.stereotype.Service;
 import team16.sidedish.domain.entity.aggregate.product.Product;
 import team16.sidedish.domain.entity.aggregate.provider.Provider;
 import team16.sidedish.domain.entity.lookUp.Category;
+import team16.sidedish.domain.enums.CategoryEnum;
 import team16.sidedish.dto.response.BestMenuResponseDto;
 import team16.sidedish.dto.response.MenuResponseDto;
+import team16.sidedish.dto.response.detail.DetailResponseDto;
 import team16.sidedish.dto.response.list.BestMenuListResponseDto;
 import team16.sidedish.dto.response.list.MenuListResponseDto;
+import team16.sidedish.exception.NotFoundException;
 import team16.sidedish.repository.ProductRepository;
 import team16.sidedish.repository.ProviderRepository;
 import team16.sidedish.repository.lookup.CategoryRepository;
+import team16.sidedish.utils.LookUpTableUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -31,61 +38,94 @@ public class ProductService {
         this.categoryRepository = categoryRepository;
     }
 
-    public MenuListResponseDto menuList() {
-        List<Product> products = (List<Product>) productRepository.findAll();
-        List<MenuResponseDto> menuResponseDtos = new ArrayList<>();
-        for (Product product : products) {
-            Provider provider = getProviderByProduct(product);
-            menuResponseDtos.add(MenuResponseDto.of(product, provider));
-        }
-        return MenuListResponseDto.of(menuResponseDtos);
+    public MenuListResponseDto getAllMenuList() {
+        List<Product> products = productRepository.findAll();
+
+        List<MenuResponseDto> menuResponseDtoList = getMenuResponseDtoList(products);
+        return MenuListResponseDto.of(menuResponseDtoList);
     }
 
-    public MenuResponseDto menu(String detailhash) {
-        Product product = productRepository.findByHash(detailhash)
+    public MenuResponseDto getMenu(String hash) {
+        Product product = productRepository.findByHash(hash)
                 .orElseThrow(IllegalStateException::new);
-        Provider provider = getProviderByProduct(product);
+        Provider provider = providerRepository.findById(product.getProviderId())
+                .orElseThrow(IllegalStateException::new);
+
         return MenuResponseDto.of(product, provider);
     }
 
-    private Provider getProviderByProduct(Product product) {
-        return providerRepository.findById(product.getProviderId())
-                .orElseThrow(IllegalStateException::new);
-    }
+    public BestMenuListResponseDto getBestAllMenuList() {
+        List<Category> categories = categoryRepository.findAllByIsBestIsTrue();
 
-    public BestMenuListResponseDto bestMenuList() {
-        List<Category> categories = categoryRepository.findAll();
-        List<MenuResponseDto> menuResponseDtos;
-        List<BestMenuResponseDto> bestMenuResponseDtos = new ArrayList<>();
-
+        List<BestMenuResponseDto> bestMenuResponseDtoList = new ArrayList<>();
         for (Category category : categories) {
-            if (category.isBest()) {
-                List<Product> products = productRepository.findAllByCategoryId(category.getId());
-                menuResponseDtos = getMenuResponseDtosByProducts(products);
-                bestMenuResponseDtos.add(BestMenuResponseDto.of(category, menuResponseDtos));
-            }
-        }
-        return BestMenuListResponseDto.of(bestMenuResponseDtos);
-    }
-
-    public BestMenuResponseDto bestMenu(Integer categoryId) {
-        Category category = categoryRepository.findById(categoryId)
-                .orElseThrow(IllegalStateException::new);
-        List<MenuResponseDto> menuResponseDtos = null;
-
-        if (category.isBest()) {
             List<Product> products = productRepository.findAllByCategoryId(category.getId());
-            menuResponseDtos = getMenuResponseDtosByProducts(products);
+
+            List<MenuResponseDto> menuResponseDtos = getMenuResponseDtoList(products);
+            bestMenuResponseDtoList.add(BestMenuResponseDto.of(category.getId(), menuResponseDtos));
         }
-        return BestMenuResponseDto.of(category, menuResponseDtos);
+
+        return BestMenuListResponseDto.of(bestMenuResponseDtoList);
     }
 
-    private List<MenuResponseDto> getMenuResponseDtosByProducts(List<Product> products) {
-        List<MenuResponseDto> menuResponseDtos = new ArrayList<>();
-        for (Product product : products) {
-            Provider provider = getProviderByProduct(product);
-            menuResponseDtos.add(MenuResponseDto.of(product, provider));
+    public BestMenuResponseDto getBestCategoryMenuList(Integer categoryId) {
+        if (!LookUpTableUtils.getCategoryById(categoryId).isBest()) {
+            throw new NotFoundException(categoryId + "는 베스트 메뉴 카테고리가 아닙니다.");
         }
-        return menuResponseDtos;
+
+        return BestMenuResponseDto.of(categoryId, getCategoryMenuList(categoryId));
+    }
+
+    private List<MenuResponseDto> getCategoryMenuList(Integer categoryId) {
+        List<Product> products = productRepository.findAllByCategoryId(categoryId);
+
+        return getMenuResponseDtoList(products);
+    }
+
+    public MenuListResponseDto getMenuList(CategoryEnum categoryEnum) {
+        return MenuListResponseDto.of(getCategoryMenuList(categoryEnum.getId()));
+    }
+
+    public MenuResponseDto getMenu(CategoryEnum categoryEnum, String hash) {
+        Product product = productRepository.findByCategoryIdAndHash(categoryEnum.getId(), hash).
+                orElseThrow(() -> new NotFoundException(hash + " 제품이 " + categoryEnum + "에 존재하지 않습니다."));
+        Provider provider = getProvider(product.getProviderId());
+
+        return MenuResponseDto.of(product, provider);
+    }
+
+    public DetailResponseDto getDetail(String hash) {
+        Product product = productRepository.findByHash(hash).
+                orElseThrow(() -> new NotFoundException(hash + " 제품이 존재하지 않습니다."));
+        Provider provider = getProvider(product.getProviderId());
+
+        return DetailResponseDto.of(product, provider);
+    }
+
+    private Provider getProvider(Long providerId) {
+        return providerRepository.findById(providerId)
+                .orElseThrow(() -> new NotFoundException(providerId + "번 업체는 존재하지 않습니다."));
+    }
+
+    private List<MenuResponseDto> getMenuResponseDtoList(List<Product> products) {
+        Map<Long, Provider> providers = getProviders(products);
+
+        List<MenuResponseDto> menuResponseDtoList = new ArrayList<>();
+        for (Product product : products) {
+            Provider provider = providers.getOrDefault(product.getProviderId(), null);
+            if (provider == null) {
+                throw new NotFoundException(product.getProviderId() + "번 업체는 존재하지 않습니다.");
+            }
+
+            menuResponseDtoList.add(MenuResponseDto.of(product, provider));
+        }
+        return menuResponseDtoList;
+    }
+
+    private Map<Long, Provider> getProviders(List<Product> products) {
+        List<Long> providerIds = products.stream().map(Product::getProviderId).collect(Collectors.toList());
+
+        return providerRepository.findAllById(providerIds).
+                stream().collect(Collectors.toMap(Provider::getId, Function.identity()));
     }
 }
