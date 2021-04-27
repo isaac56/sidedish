e200b5237dfe8a9aebbf0f01e819aa15237d9675
정이삭 <isaac56@naver.com>
2021-04-27 14:56:47 +0900

    [#21] feat: Create ApiResult
    제너릭을 사용해 공통형식으로 Rest API를 반환해주는 ApiResult를 만들었습니다.

---
+++ BE/sidedish/src/main/java/team16/sidedish/dto/response/ApiResult.java
@@ -0,0 +1,31 @@
+package team16.sidedish.dto.response;
+
+public class ApiResult<T> {
+    private T data;
+    private String error;
+
+    private ApiResult(T data, String error) {
+        this.data = data;
+        this.error = error;
+    }
+
+    public static <T> ApiResult<T> succeed(T data) {
+        return new ApiResult(data, null);
+    }
+
+    public static ApiResult<?> failed(String errorMessage) {
+        return new ApiResult<>(null, errorMessage);
+    }
+
+    public static ApiResult<?> failed(Throwable throwable) {
+        return failed(throwable.getMessage());
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
