26d4a974e09c47821f15700b56d9be2b65f4957d
정이삭 <isaac56@naver.com>
2021-04-29 18:29:28 +0900

    [#33] feat: Add necessary method in UserRepository

--- BE/sidedish/src/main/java/team16/sidedish/repository/UserRepository.java
+++ BE/sidedish/src/main/java/team16/sidedish/repository/UserRepository.java
@@ -3,5 +3,10 @@ package team16.sidedish.repository;
 import org.springframework.data.repository.CrudRepository;
 import team16.sidedish.domain.entity.aggregate.user.User;
 
-public interface UserRepository extends CrudRepository<User,Long> {
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 }
